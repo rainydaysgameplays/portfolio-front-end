167773de1b9fccc437ba34e8b1f06e4caae2ff77
rainydaysgameplays <rainydaysgameplays@gmail.com>
2021-09-15 22:12:37 -0500

    Carousel outlined

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { useState } from 'react';
 
 export default function Carousel({ pictures }) {
+
     
-    console.log(pictures[0])
+    
+    console.log(pictures)
     return (
-        <div>
-            <img className="Simon-Page-img" src={pictures[0]} alt="project pictures" />
+        <div id="Carousel-container">
+            <div id="Carousel-left">
+                {/* causes the carousel to move to the left */}
+                <p>left</p>
+            </div>
+
+            <img className="Simon-Page-img" src={pictures[1]} alt="project pictures" />
+            
+            <div id="Carousel-right">
+                <p>right</p>
+            </div>
         </div>
     );
 }
