d6847e6555445f8cb1197d5620946c252b171400
rainydaysgameplays <rainydaysgameplays@gmail.com>
2021-09-17 21:27:40 -0500

    onclick functions working

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -3,22 +3,41 @@ import { useState } from 'react';
 
 export default function Carousel({ pictures }) {
 
-    
+    // declaring state for pictures
+    const [picState, setPicState] = useState(0)
     
     console.log(pictures)
+
+    function leftClick(){
+        if(picState === 0){
+            setPicState(pictures.length -1)
+        }
+        else(setPicState(picState-1))
+    }
+
+    function rightClick(){
+        if(picState === pictures.length -1){
+            setPicState(0)
+        }
+        else(setPicState(picState+1))
+    }
+
+
     return (
         <div id="Carousel-container">
             <div id="Carousel-left">
-                {/* causes the carousel to move to the left */}
-                <p>left</p>
+                {/* onclick function to set state -1 and to loop through */}
+                <p>{"<<"}</p>
             </div>
 
-            <img className="Simon-Page-img" src={pictures[1]} alt="project pictures" />
+            <img className="Simon-Page-img" src={pictures[picState]} alt="project pictures" />
             
             <div id="Carousel-right">
-                <p>right</p>
+                <p>{">>"}</p>
             </div>
+
         </div>
+        
     );
 }
 
